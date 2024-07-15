54c2c1e726622d42cff90f6926f538412d30c0bd
Jeffrey van Norden <jeffrey@grexx.net>
2024-07-15 11:01:55 +0200

    Run errants

--- custom_components/one2track/client/__init__.py
+++ custom_components/one2track/client/__init__.py
@@ -1,5 +1,5 @@
 from .gps_client import GpsClient
-from .client_types import One2TrackConfig
+from .client_types import One2TrackConfig, AuthenticationError, TrackerDevice
 
 
 def get_client(config: One2TrackConfig) -> GpsClient:
---
+++ custom_components/one2track/common.py
@@ -0,0 +1,9 @@
+DOMAIN = "one2track"
+DEFAULT_PREFIX = "one2track"
+DEFAULT_UPDATE_RATE_MIN = 30
+
+# Config keys
+CONF_USER_NAME = "Username"
+CONF_PASSWORD = "Password"
+CONF_ID = "AccountID"
+
